b6f1771d963e17b9cb7c827dbdfccd625948fc05
Yurii <yra-g@ukr.net>
2024-10-30 23:47:57 +0200

    💪 Complete task 4

---
+++ task_4.py
@@ -0,0 +1,69 @@
+import heapq
+import uuid
+
+import networkx as nx
+import matplotlib.pyplot as plt
+from fontTools.misc.cython import returns
+
+
+class Node:
+    def __init__(self, key, color="skyblue"):
+        self.left = None
+        self.right = None
+        self.val = key
+        self.color = color  # Додатковий аргумент для зберігання кольору вузла
+        self.id = str(uuid.uuid4())  # Унікальний ідентифікатор для кожного вузла
+
+
+def add_edges(graph, node, pos, x=0, y=0, layer=1):
+    if node is not None:
+        graph.add_node(node.id, color=node.color, label=node.val)  # Використання id та збереження значення вузла
+        if node.left:
+            graph.add_edge(node.id, node.left.id)
+            l = x - 1 / 2 ** layer
+            pos[node.left.id] = (l, y - 1)
+            l = add_edges(graph, node.left, pos, x=l, y=y - 1, layer=layer + 1)
+        if node.right:
+            graph.add_edge(node.id, node.right.id)
+            r = x + 1 / 2 ** layer
+            pos[node.right.id] = (r, y - 1)
+            r = add_edges(graph, node.right, pos, x=r, y=y - 1, layer=layer + 1)
+    return graph
+
+
+def draw_tree(tree_root):
+    tree = nx.DiGraph()
+    pos = {tree_root.id: (0, 0)}
+    tree = add_edges(tree, tree_root, pos)
+
+    colors = [node[1]['color'] for node in tree.nodes(data=True)]
+    labels = {node[0]: node[1]['label'] for node in tree.nodes(data=True)}  # Використовуйте значення вузла для міток
+
+    plt.figure(figsize=(8, 5))
+    nx.draw(tree, pos=pos, labels=labels, arrows=False, node_size=2500, node_color=colors)
+    plt.show()
+
+
+def render_heap_tree(heap: list, i=0, node: Node = None) -> Node:
+    if node is None:
+        node = Node(heap[i])
+
+    # Рекурсивно формуємо дерево
+    left = 2 * i + 1
+    right = 2 * i + 2
+    if len(heap) > left:
+        node.left = Node(heap[left])
+        render_heap_tree(heap, left, node.left)
+
+    if len(heap) > right:
+        node.right = Node(heap[right])
+        render_heap_tree(heap, right, node.right)
+
+    # Перевіряєм чи це кореневий елмент і рендеримо створене дерево
+    if i == 0:
+        draw_tree(node)
+
+
+heap = [10, 5, 3, 4, 1]
+
+render_heap_tree(heap)
